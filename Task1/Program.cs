2ed1f9bc421dac1d6dae673058ae74513036ebfa
Kael <churushkin.kirill@mail.ru>
2022-11-14 17:48:48 +0300

    COMMIT

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int MultNumbers(int num1, int num2)
+{
+    int number = num1;
+        for (int count = 1; count < num2; count++)
+        {
+            number *= num1;
+        }
+    return number;
+}
+if(b > 0 )
+{
+int multiply = MultNumbers(a, b);
+Console.WriteLine($"Число {a} в степени {b} равно --> {multiply}");
+}
+
+else Console.WriteLine("Введена неверная степень числа");
---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("Введите число");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int num1)
+{
+    int number = 0;
+    // int num2 = num1;
+    while (num1 > 0)
+    {
+        number += num1 % 10 ;
+        Console.WriteLine($"{num1}");
+        num1 /= 10;
+
+    }
+
+    return number;
+}
+int sum = SumDigits(a);
+Console.WriteLine($"Сумма цифр в числе {a} равно --> {sum}");
